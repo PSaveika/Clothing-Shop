f61c42a4d3516f172b58719e52c1f0fe9e589b67
psaveika <povilassaveika@gmail.com>
2022-09-10 22:25:05 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,54 @@
+﻿const menu = document.getElementById('mobile-button');
+const close = document.getElementById('close');
+const nav = document.querySelector('.navbar-list')
+
+const navBG = document.querySelector("nav");
+
+if (menu) {
+    menu.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+window.onscroll = () => {
+    if(window.scrollY >= 100) {
+        navBG.classList.add('nav-active');
+    } else {
+        navBG.classList.remove('nav-active');
+    }
+}
+// 
+var mainImg = document.querySelector(".main-img");
+var smallerImg = document.getElementsByClassName("smaller-img");
+
+for(var img of smallerImg) {
+    img.addEventListener("click", function(){
+        mainImg.src = this.src;
+    });
+}
+// 
+const toProduct = document.getElementsByClassName("to-product");
+let imgUrl;
+
+for(var toProd of toProduct) {
+    toProd.addEventListener('click', function(){
+        imgUrl = this.getElementsByTagName('img')[0];
+        imgUrl = imgUrl.src;
+        localStorage.setItem("imgUrlValue", imgUrl);
+        window.location.href='product.html';
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    if(!mainImg) {
+        return
+    } else {
+        mainImg.src = localStorage.getItem("imgUrlValue")
+    }
+})
